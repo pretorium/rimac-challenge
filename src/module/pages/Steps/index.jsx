393290edb64ff8926d05e6ab06f4f11f9bcd08ad
yiomar <yruiz@solera.pe>
2021-06-18 16:36:10 -0500

    - Layout en index de steps

--- src/module/pages/Steps/index.jsx
+++ src/module/pages/Steps/index.jsx
@@ -3,17 +3,25 @@ import { Switch } from 'react-router-dom';
 import RouterOutlet from 'shared/RouterOutlet';
 import Header from 'shared/Header';
 import routes from './routes';
+import './styles.scss';
 
 function Steps() {
   return (
-    <div>
+    <>
       <Header />
-      <Switch>
-        {routes.map((route) => (
-          <RouterOutlet key={route.name} {...route} />
-        ))}
-      </Switch>
-    </div>
+      <div className="layout container">
+        <div className="layout__stepCounter">
+          steps
+        </div>
+        <div className="layout__content">
+          <Switch>
+            {routes.map((route) => (
+              <RouterOutlet key={route.name} {...route} />
+            ))}
+          </Switch>
+        </div>
+      </div>
+    </>
   );
 }
 
