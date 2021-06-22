140a4abc330b75617e85dcd0a71294bbeb0c0afe
yiomar <yruiz@solera.pe>
2021-06-22 13:18:28 -0500

    - Reajuste de mixin

--- src/shared/NotFound/index.jsx
+++ src/shared/NotFound/index.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 function NotFound() {
   return (
-    <div>
-      not found
-    </div>
+    <img
+      alt="notFound"
+      width="250px"
+      height="250px"
+      style={{ margin: 'auto' }}
+      src="https://cdn.iconscout.com/icon/free/png-256/data-not-found-1965034-1662569.png"
+    />
   );
 }
 
